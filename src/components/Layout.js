f0579acd25c9a29fd52a1c49866d7fff25a85c46
stormwild <artorrijos@gmail.com>
2021-10-04 09:27:54 +0000

    Added hello script

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -53,7 +53,8 @@ export default class Body extends React.Component {
                     <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
-                    <link rel="manifest" href="/images/favicon/site.webmanifest"></link>
+              <link rel="manifest" href="/images/favicon/site.webmanifest"></link>
+              <script>console.log('hello')</script>
                     <body className={'font-' + _.get(this.props, 'pageContext.site.siteMetadata.base_font', null) + ' palette-' + _.get(this.props, 'pageContext.site.siteMetadata.color_scheme', null) + ' accent-' + _.get(this.props, 'pageContext.site.siteMetadata.accent_color', null)} />
                 </Helmet>
                 <div id="site-wrap" className="site">
